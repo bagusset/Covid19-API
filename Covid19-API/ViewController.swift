4a6f25f046393f90d6e84e01b8ac49537245d2ca
Bagus setiawan <setiawanbagus4315@gmail.com>
2021-01-21 21:55:48 +0700

    setting networking API !

--- Covid19-API/ViewController.swift
+++ Covid19-API/ViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var apiServices = ApiService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        apiServices.getDataReportCovid19 { (Countries) in
+            print(Countries)
+        }
         // Do any additional setup after loading the view.
     }
 
